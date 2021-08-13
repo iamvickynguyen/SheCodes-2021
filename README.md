ab5b6cf0bd6f663dc924dfeb775aa7c1d6c82954
Vicky Nguyen <44889544+VickyNguyen99@users.noreply.github.com>
2021-08-13 15:18:37 -0700

    add boilerplate chat

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 reference: https://codepen.io/colorlib/pen/rxddKy
 
 https://codepen.io/RobVermeer/pen/japZpY
+
+https://codepen.io/greg-forster/pen/BxGypN
+
+https://codepen.io/Momciloo/pen/bEdbxY
+
+https://codepen.io/ThomasDaubenton/pen/QMqaBN
--- app.py
+++ app.py
@@ -6,14 +6,16 @@ app = Flask(__name__)
 def login():
     if request.method == 'POST':
         # TODO
-        people = [('static/img/i1.jpg', 'Person 1', 'some descriptions'),
-                    ('static/img/i2.jpg', 'Person 2', 'some descriptions'),
-                    ('static/img/i3.jpg', 'Person 3', 'some descriptions'),
-                    ('static/img/i4.jpg', 'Person 4', 'some descriptions'),
-                    ('static/img/i5.jpg', 'Person 5', 'some descriptions'),
-                    ('static/img/i6.jpg', 'Person 6', 'some descriptions')]
+        people = [('Person 1', 'girl icon', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+                    ('Person 2', 'girl icon', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+                    ('Person 3', 'girl icon', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+                ]
         return render_template('home.html', people = people)
     return render_template('login.html')
+
+@app.route('/messages', methods=['GET', 'POST'])
+def messages():
+    return render_template('chat.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
